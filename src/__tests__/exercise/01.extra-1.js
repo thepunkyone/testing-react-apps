46d8d0b369ccd5d5c1a07f47016162328ebef82f
Whitney Bluzma <whitney@accommodationforstudents.com>
2023-10-26 13:40:54 +0100

    feat: 1 - simple test setup with React DOM

---
+++ src/__tests__/exercise/01.extra-1.js
@@ -0,0 +1,46 @@
+// simple test with ReactDOM
+// http://localhost:3000/counter
+
+import * as React from 'react'
+import {act} from 'react-dom/test-utils'
+import {createRoot} from 'react-dom/client'
+import Counter from '../../components/counter'
+
+// NOTE: this is a new requirement in React 18
+// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
+// Luckily, it's handled for you by React Testing Library :)
+global.IS_REACT_ACT_ENVIRONMENT = true
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
+const mouseEvent = new MouseEvent('click', {
+  bubbles: true,
+  cancelable: true,
+  button: 0,
+})
+
+test('counter increments and decrements when the buttons are clicked', () => {
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+
+  const [decrement, increment] = div.querySelectorAll('button')
+
+  const message = div.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count: 0')
+
+  act(() => increment.dispatchEvent(mouseEvent))
+
+  expect(message.textContent).toBe('Current count: 1')
+
+  act(() => decrement.dispatchEvent(mouseEvent))
+
+  expect(message.textContent).toBe('Current count: 0')
+})
+
+/* eslint no-unused-vars:0 */
