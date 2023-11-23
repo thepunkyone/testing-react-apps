70af694eeadd3b92378c6c8451ac2ce558ed4074
Whitney Bluzma <whitney@accommodationforstudents.com>
2023-11-23 11:46:05 +0000

    feat: exercise 6 extra credit: mocking a package

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -2,42 +2,30 @@
 // http://localhost:3000/location
 
 import * as React from 'react'
-import {render, screen, act, waitFor} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
+import {useCurrentPosition} from 'react-use-geolocation'
 import Location from '../../examples/location'
 
-function deferred() {
-  let resolve, reject
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-  return {promise, resolve, reject}
-}
+jest.mock('react-use-geolocation')
 
-beforeAll(() => {
-  window.navigator.geolocation = {
-    getCurrentPosition: jest.fn(),
-  }
-})
-
-test('displays the users current location', async () => {
+test('displays the users current location when the position is retrieved successfully', async () => {
   const fakePosition = {coords: {latitude: 54, longitude: -2}}
 
-  const {promise, resolve} = deferred()
+  let setReturnValue
 
-  window.navigator.geolocation.getCurrentPosition.mockImplementation(
-    callback => {
-      promise.then(() => callback(fakePosition))
-    },
-  )
+  useCurrentPosition.mockImplementation(() => {
+    const state = React.useState([])
+    setReturnValue = state[1]
+
+    return state[0]
+  })
 
   render(<Location />)
 
   expect(screen.getByLabelText(/loading/)).toBeVisible()
 
-  await act(async () => {
-    resolve()
-    await promise
+  await act(() => {
+    setReturnValue([fakePosition])
   })
 
   expect(screen.queryByLabelText(/loading/)).not.toBeInTheDocument()
@@ -48,6 +36,31 @@ test('displays the users current location', async () => {
   expect(screen.getByText(/longitude/i)).toHaveTextContent(
     `Longitude: ${fakePosition.coords.longitude}`,
   )
+})
+
+test('displays an error when the users position could not be retrieved successfully', async () => {
+  const fakeError = {message: 'Could not retrieve coordinates!'}
+
+  let setReturnValue
+
+  useCurrentPosition.mockImplementation(() => {
+    const state = React.useState([])
+    setReturnValue = state[1]
+
+    return state[0]
+  })
+
+  render(<Location />)
+
+  expect(screen.getByLabelText(/loading/)).toBeVisible()
+
+  await act(() => {
+    setReturnValue([undefined, fakeError])
+  })
+
+  expect(screen.queryByLabelText(/loading/)).not.toBeInTheDocument()
+
+  expect(screen.getByRole('alert')).toHaveTextContent(fakeError.message)
 })
 
 /*
